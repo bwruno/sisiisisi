09d037691918929053d88119f75d0129d9caf79a
bwruno <brunoelcobra@gmail.com>
2024-05-13 22:08:17 +0200

    Lo he cambiado todo manin

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,6 @@ public class Main {
             System.out.println("i = " + i);
         }
         System.out.println("ME duelen los dientes");
-        System.out.println("dios mio mi edglan");
+        System.out.println("dios mio mi edglan!");
     }
 }
